820ec5d0b07b99f9961989bdf068220570e194d3
Steven <stevenjkuntz@gmail.com>
2019-08-07 13:53:26 -0700

    pyvenv integeration
    I added out-of-the-box support for `pyvenv'. This was tested with
    `pyvenv-workon' but should work any time `pyvenv-activate' is called. The README
    is updated to reflect this change.

--- packages.el
+++ packages.el
@@ -33,10 +33,11 @@
 (defconst jupyter-packages
   '(
     company
+    jupyter
     (ox-ipynb :location (recipe
                          :fetcher github
                          :repo "jkitchin/ox-ipynb"))
-    jupyter
+    pyvenv
     )
   )
 
@@ -44,74 +45,32 @@
   ;; Enable company-capf backend in the REPL.
   (spacemacs|add-company-hook jupyter-repl-mode)
   (push 'company-capf company-backends-jupyter-repl-mode)
-  ;; (spacemacs|add-company-backends
-  ;;  :backends company-capf
-  ;;  :modes jupyter-repl-mode)
-  )
-
-;; (defun jupyter/init-company-capf ()
-;;   (use-package company-capf
-;;     :defer t
-;;     :init (push 'company-capf company-backends-jupyter-repl-mode)
-;;     )
-;;   )
-
-(defun jupyter/init-ox-ipynb ()
-  (use-package ox-ipynb)
-  )
-
-(defun jupyter/post-init-ox-ipynb ()
-  ;; jupyter-LANG needs to point to LANG's kernelspecs.
-  ;; TODO: Pull version info from a dynamic source.
-  (add-to-list 'ox-ipynb-kernelspecs
-               '(jupyter-python . (kernelspec . ((display_name . "Python 3")
-                                                 (language . "python")
-                                                 (name . "python3"))))
-               )
-  (add-to-list 'ox-ipynb-kernelspecs
-               '(jupyter-julia . (kernelspec . ((display_name . "Julia 0.6.0")
-                                                (language . "julia")
-                                                (name . "julia-0.6"))))
-               )
-
-  ;; jupyter-LANG needs to point to LANG's metadata.
-  ;; TODO: Pull version info from a dynamic source.
-  (add-to-list 'ox-ipynb-language-infos
-               '(jupyter-python . (language_info . ((codemirror_mode . ((name . ipython)
-                                                                        (version . 3)))
-                                                    (file_extension . ".py")
-                                                    (mimetype . "text/x-python")
-                                                    (name . "python")
-                                                    (nbconvert_exporter . "python")
-                                                    (pygments_lexer . "ipython3")
-                                                    (version . "3.5.2"))))
-               )
-  (add-to-list 'ox-ipynb-language-infos
-               '(jupyter-julia . (language_info . ((codemirror_mode . "julia")
-                                                   (file_extension . ".jl")
-                                                   (mimetype . "text/x-julia")
-                                                   (name . "julia")
-                                                   (pygments_lexer . "julia")
-                                                   (version . "0.6.0"))))
-               )
   )
 
 (defun jupyter/init-jupyter ()
+  ;; Load `jupyter'
   (if (executable-find "jupyter")
       (use-package jupyter
         :defer t
         :init
         (progn
+          ;; basic key bindings
           (spacemacs/set-leader-keys
             "ajj" 'jupyter-run-repl
             "ajr" 'jupyter-run-repl
             "ajc" 'jupyter-connect-repl)
-          ;; (spacemacs|add-company-backends :backends company :modes jupyter-repl-mode)
           )
-        ;; :config
-        ;; (progn
-        ;;   (evilified-state-evilify-map 'jupyter-repl-mode-map
-        ;;     :mode jupyter-repl-mode)
+        :config
+        (progn
+          ;; evil key bindings
+          (evilified-state-evilify-map 'jupyter-repl-mode-map
+            :mode jupyter-repl-mode)
+
+          ;; make sure pyvenv affects jupyter
+          ;; (when (package-installed-p 'pyvenv)
+          ;;   (add-hook 'pyvenv-post-activate-hooks (lambda() (jupyter-available-kernelspecs t)))
+          ;;   (add-hook 'pyvenv-post-activate-hooks (lambda() (jupyter-available-kernelspecs t)))
+          ;;   )
 
         ;;   (spacemacs/declare-prefix-for-mode 'jupyter-repl-mode
         ;;     "mf" "file")
@@ -133,10 +92,64 @@
         ;;     "hN"  'jupyter-repl-history-previous
         ;;     "i" 'jupyter-inspect-at-point
         ;;     "sb" 'jupyter-repl-pop-to-buffer)
-        ;;   )
+          )
         )
     (message "jupyter was not found in your path, jupyter is not loaded")
     )
+  )
+
+(defun jupyter/init-ox-ipynb ()
+  ;; Load `ox-ipynb'
+  (use-package ox-ipynb
+    :config
+    (progn
+      ;; jupyter-LANG needs to point to LANG's kernelspecs.
+      ;; TODO: Pull version info from a dynamic source.
+      (add-to-list 'ox-ipynb-kernelspecs
+                   '(jupyter-python . (kernelspec . (
+                                                     (display_name . "Python 3")
+                                                     (language . "python")
+                                                     (name . "python3")
+                                                     )))
+                   )
+      (add-to-list 'ox-ipynb-kernelspecs
+                   '(jupyter-julia . (kernelspec . (
+                                                    (display_name . "Julia 1.1")
+                                                    (language . "julia")
+                                                    (name . "julia-1.1")
+                                                    )))
+                   )
+
+      ;; jupyter-LANG needs to point to LANG's metadata.
+      ;; TODO: Pull version info from a dynamic source.
+      (add-to-list 'ox-ipynb-language-infos
+                   '(jupyter-python . (language_info . (
+                                                        (codemirror_mode . ((name . ipython)
+                                                                            (version . 3)))
+                                                        (file_extension . ".py")
+                                                        (mimetype . "text/x-python")
+                                                        (name . "python")
+                                                        (nbconvert_exporter . "python")
+                                                        (pygments_lexer . "ipython3")
+                                                        (version . "3.7")
+                                                        )))
+                   )
+      (add-to-list 'ox-ipynb-language-infos
+                   '(jupyter-julia . (language_info . (
+                                                       (codemirror_mode . "julia")
+                                                       (file_extension . ".jl")
+                                                       (mimetype . "text/x-julia")
+                                                       (name . "julia")
+                                                       (pygments_lexer . "julia")
+                                                       (version . "1.1")
+                                                       )))
+                   )
+      )
+    )
+  )
+
+(defun jupyter/post-init-pyvenv ()
+  (add-hook 'pyvenv-post-activate-hooks (lambda() (jupyter-available-kernelspecs t)))
   )
 
 ;;; packages.el ends here
